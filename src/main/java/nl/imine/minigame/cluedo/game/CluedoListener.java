d7d2085e69f52823a3574c7a24854e89cfd9b00c
Sansko1337 <sanderknauff@hotmail.com>
2017-03-18 23:59:34 +0100

    Reformat CluedoListener

--- src/main/java/nl/imine/minigame/cluedo/game/CluedoListener.java
+++ src/main/java/nl/imine/minigame/cluedo/game/CluedoListener.java
@@ -22,71 +22,70 @@ import org.bukkit.inventory.ItemStack;
 
 public class CluedoListener implements Listener {
 
-	public static void init() {
-		Bukkit.getServer().getPluginManager().registerEvents(new CluedoListener(), CluedoPlugin.getInstance());
-	}
+    public static void init() {
+        Bukkit.getServer().getPluginManager().registerEvents(new CluedoListener(), CluedoPlugin.getInstance());
+    }
 
-	@EventHandler(priority = EventPriority.LOW)
-	public void onPlayerDamage(EntityDamageEvent evt) {
-		//Check if entity is a player
-		if (!(evt.getEntity() instanceof Player)) {
-			return;
-		}
-		Player player = (Player) evt.getEntity();
+    @EventHandler(priority = EventPriority.LOW)
+    public void onPlayerDamage(EntityDamageEvent evt) {
+        //Check if entity is a player
+        if (!(evt.getEntity() instanceof Player)) {
+            return;
+        }
+        Player player = (Player) evt.getEntity();
 
-		//Make sure the player is actually participating in this minigame
-		if (!CluedoPlugin.getGame().getPlayers().contains(player)) {
-			return;
-		}
+        //Make sure the player is actually participating in this minigame
+        if (!CluedoPlugin.getGame().getPlayers().contains(player)) {
+            return;
+        }
 
-		//Turn off falling damage
-		if (evt.getCause().equals(EntityDamageEvent.DamageCause.FALL)) {
-			evt.setCancelled(true);
-		}
-	}
+        //Turn off falling damage
+        if (evt.getCause().equals(EntityDamageEvent.DamageCause.FALL)) {
+            evt.setCancelled(true);
+        }
+    }
 
-	@EventHandler
-	public void onPlayerDead(PlayerDeathEvent evt) {
-		Player player = evt.getEntity();
+    @EventHandler
+    public void onPlayerDead(PlayerDeathEvent evt) {
+        Player player = evt.getEntity();
 
-		//Make sure the player is actually participating in this minigame
-		if (!CluedoPlugin.getGame().getPlayers().contains(player)) {
-			return;
-		}
+        //Make sure the player is actually participating in this minigame
+        if (!CluedoPlugin.getGame().getPlayers().contains(player)) {
+            return;
+        }
 
-		//Get Cluedo player object
-		CluedoPlayer cluedoPlayer = CluedoPlugin.getGame().getCluedoPlayers().stream()
-				.filter(cPlayer -> cPlayer.getPlayer().equals(player))
-				.findFirst().orElse(null);
+        //Get Cluedo player object
+        CluedoPlayer cluedoPlayer = CluedoPlugin.getGame().getCluedoPlayers().stream()
+                .filter(cPlayer -> cPlayer.getPlayer().equals(player))
+                .findFirst().orElse(null);
 
-		//Handle item drops
-		if(cluedoPlayer.getRole().getRoleType().equals(RoleType.DETECTIVE)){
-			evt.getEntity().getLocation().getWorld()
-					.dropItem(evt.getEntity().getLocation(), new ItemStack(Material.BOW));
-		}
-		evt.getDrops().clear();
+        //Handle item drops
+        if (cluedoPlayer.getRole().getRoleType().equals(RoleType.DETECTIVE)) {
+            evt.getEntity().getLocation().getWorld()
+                    .dropItem(evt.getEntity().getLocation(), new ItemStack(Material.BOW));
+        }
+        evt.getDrops().clear();
 
-		CluedoPlugin.getGame().getGameState().handlePlayerDeath(player);
-		player.spigot().respawn();
-	}
+        CluedoPlugin.getGame().getGameState().handlePlayerDeath(player);
+        player.spigot().respawn();
+    }
 
-	@EventHandler
-	public void onPlayerRespawn(PlayerRespawnEvent evt){
-		Player player = evt.getPlayer();
+    @EventHandler
+    public void onPlayerRespawn(PlayerRespawnEvent evt) {
+        Player player = evt.getPlayer();
 
-		//Make sure the player is actually participating in this minigame
-		if (!CluedoPlugin.getGame().getPlayers().contains(player)) {
-			return;
-		}
-
+        //Make sure the player is actually participating in this minigame
+        if (!CluedoPlugin.getGame().getPlayers().contains(player)) {
+            return;
+        }
 
 
         //TODO HANDLE RESPAWN (TELEPORT, ETC)
 
-	}
+    }
 
     @EventHandler
-    public void onPlayerItemPickup(PlayerPickupItemEvent evt){
+    public void onPlayerItemPickup(PlayerPickupItemEvent evt) {
         Player player = evt.getPlayer();
 
         //Make sure the player is actually participating in this minigame
@@ -100,7 +99,7 @@ public class CluedoListener implements Listener {
                 .findFirst().orElse(null);
 
         //Check if the player is a bystander without a weapon
-        if(cluedoPlayer.getRole().getRoleType().equals(RoleType.BYSTANDER)){
+        if (cluedoPlayer.getRole().getRoleType().equals(RoleType.BYSTANDER)) {
             cluedoPlayer.setRole(RoleType.DETECTIVE);
         }
 
@@ -108,122 +107,122 @@ public class CluedoListener implements Listener {
         evt.setCancelled(true);
     }
 
-	@EventHandler(priority = EventPriority.HIGH)
-	public void onPvPDamage(EntityDamageByEntityEvent evt) {
-		//Check if entity is a player
-		if (!(evt.getEntity() instanceof Player)) {
-			return;
-		}
-		Player player = (Player) evt.getEntity();
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onPvPDamage(EntityDamageByEntityEvent evt) {
+        //Check if entity is a player
+        if (!(evt.getEntity() instanceof Player)) {
+            return;
+        }
+        Player player = (Player) evt.getEntity();
 
-		//Check if the attacker was a player
-		Player damager = null;
-		boolean wasArrow = false;
-		if (!(evt.getDamager() instanceof Player)) {
-			if (evt.getDamager() instanceof Arrow) {
-				Arrow arrow = (Arrow) evt.getDamager();
-				if (arrow.getShooter() instanceof Player) {
-					damager = (Player) arrow.getShooter();
-					wasArrow = true;
-				}
-			} else {
-				return;
-			}
-		}
-		if (damager == null) {
-			damager = (Player) evt.getDamager();
-		}
-
-		//Make sure the players are actually participating in this minigame
-		if (!(CluedoPlugin.getGame().getPlayers().contains(damager) && CluedoPlugin.getGame().getPlayers().contains(player))) {
-			return;
-		}
-
-		if(CluedoPlugin.getGame().getGameState().getState().equals(CluedoStateType.IN_GAME)) {
-			if (wasArrow) {
-				handleArrowDamage(evt);
-			} else {
-				handleMeleeDamage(evt);
-			}
-		} else {
-			evt.setCancelled(true);
-		}
-	}
-
-	private void handleMeleeDamage(EntityDamageByEntityEvent evt) {
-		double woodenSwordMaxDamage = 4;
-		if (evt.getDamage() >= woodenSwordMaxDamage) {
-			evt.setDamage(100);
-		} else {
-			evt.setCancelled(true);
-		}
-	}
-
-	private void handleArrowDamage(EntityDamageByEntityEvent evt) {
-		Arrow arrow = (Arrow) evt.getDamager();
-		if (arrow.isCritical()) {
-			evt.setDamage(100);
-		} else {
-			evt.setCancelled(true);
-		}
-	}
-
-
-        @EventHandler
-        private void onPlayerItemDrop(PlayerDropItemEvent pdie){
-
-                //Make sure the player is actually participating in this minigame
-                if(!CluedoPlugin.getGame().getPlayers().contains(pdie.getPlayer())){
-                    return;
+        //Check if the attacker was a player
+        Player damager = null;
+        boolean wasArrow = false;
+        if (!(evt.getDamager() instanceof Player)) {
+            if (evt.getDamager() instanceof Arrow) {
+                Arrow arrow = (Arrow) evt.getDamager();
+                if (arrow.getShooter() instanceof Player) {
+                    damager = (Player) arrow.getShooter();
+                    wasArrow = true;
                 }
-
-                if(pdie.getPlayer().getGameMode() != GameMode.CREATIVE){
-                    pdie.setCancelled(true);
-                }
+            } else {
+                return;
+            }
+        }
+        if (damager == null) {
+            damager = (Player) evt.getDamager();
         }
 
+        //Make sure the players are actually participating in this minigame
+        if (!(CluedoPlugin.getGame().getPlayers().contains(damager) && CluedoPlugin.getGame().getPlayers().contains(player))) {
+            return;
+        }
 
-        @EventHandler
-        private void onPlayerInteract(PlayerInteractEvent pie) {
-
-                //Make sure the player is actually participating in this minigame
-                if(!CluedoPlugin.getGame().getPlayers().contains(pie.getPlayer())){
-                    return;
-                }
-
-                //people in Creative get full access to edit the map
-                if (pie.getPlayer().getGameMode() == GameMode.CREATIVE) {
-                   return;
-                }
-
-                //disallow every interaction except stone buttons and levers
-                if (pie.getClickedBlock() == null || pie.getClickedBlock().getType() == Material.STONE_BUTTON || pie.getClickedBlock().getType() == Material.WOOD_BUTTON || pie.getClickedBlock().getType() == Material.LEVER) {
-                    return;
-                }
-
-                pie.setCancelled(true);
+        if (CluedoPlugin.getGame().getGameState().getState().equals(CluedoStateType.IN_GAME)) {
+            if (wasArrow) {
+                handleArrowDamage(evt);
+            } else {
+                handleMeleeDamage(evt);
+            }
+        } else {
+            evt.setCancelled(true);
+        }
     }
 
-        @EventHandler
-        private void onPotionConsume(PlayerItemConsumeEvent pice) {
+    private void handleMeleeDamage(EntityDamageByEntityEvent evt) {
+        double woodenSwordMaxDamage = 4;
+        if (evt.getDamage() >= woodenSwordMaxDamage) {
+            evt.setDamage(100);
+        } else {
+            evt.setCancelled(true);
+        }
+    }
 
-                //Make sure the player is actually participating in this minigame
-                if(!CluedoPlugin.getGame().getPlayers().contains(pice.getPlayer())){
-                    return;
-                }
+    private void handleArrowDamage(EntityDamageByEntityEvent evt) {
+        Arrow arrow = (Arrow) evt.getDamager();
+        if (arrow.isCritical()) {
+            evt.setDamage(100);
+        } else {
+            evt.setCancelled(true);
+        }
+    }
 
-                //when a player in Creative dirnks a potion he doesn't get a bottle, so no need to do anything else
-                if (pice.getPlayer().getGameMode() == GameMode.CREATIVE) {
-                    return;
-                }
 
-                if (pice.getItem().getType() == Material.POTION) {
-                    Bukkit.getScheduler().scheduleSyncDelayedTask(CluedoPlugin.getInstance(), () -> { //delay to allow the bottle to be placed into the inventory
-                        ItemStack bottle = new ItemStack(Material.GLASS_BOTTLE);
-                        bottle.setAmount(1);
-                        pice.getPlayer().getInventory().remove(bottle);
-                    }, 1L);
-                }
+    @EventHandler
+    private void onPlayerItemDrop(PlayerDropItemEvent pdie) {
+
+        //Make sure the player is actually participating in this minigame
+        if (!CluedoPlugin.getGame().getPlayers().contains(pdie.getPlayer())) {
+            return;
+        }
+
+        if (pdie.getPlayer().getGameMode() != GameMode.CREATIVE) {
+            pdie.setCancelled(true);
+        }
+    }
+
+
+    @EventHandler
+    private void onPlayerInteract(PlayerInteractEvent pie) {
+
+        //Make sure the player is actually participating in this minigame
+        if (!CluedoPlugin.getGame().getPlayers().contains(pie.getPlayer())) {
+            return;
+        }
+
+        //people in Creative get full access to edit the map
+        if (pie.getPlayer().getGameMode() == GameMode.CREATIVE) {
+            return;
+        }
+
+        //disallow every interaction except stone buttons and levers
+        if (pie.getClickedBlock() == null || pie.getClickedBlock().getType() == Material.STONE_BUTTON || pie.getClickedBlock().getType() == Material.WOOD_BUTTON || pie.getClickedBlock().getType() == Material.LEVER) {
+            return;
+        }
+
+        pie.setCancelled(true);
+    }
+
+    @EventHandler
+    private void onPotionConsume(PlayerItemConsumeEvent pice) {
+
+        //Make sure the player is actually participating in this minigame
+        if (!CluedoPlugin.getGame().getPlayers().contains(pice.getPlayer())) {
+            return;
+        }
+
+        //when a player in Creative dirnks a potion he doesn't get a bottle, so no need to do anything else
+        if (pice.getPlayer().getGameMode() == GameMode.CREATIVE) {
+            return;
+        }
+
+        if (pice.getItem().getType() == Material.POTION) {
+            Bukkit.getScheduler().scheduleSyncDelayedTask(CluedoPlugin.getInstance(), () -> { //delay to allow the bottle to be placed into the inventory
+                ItemStack bottle = new ItemStack(Material.GLASS_BOTTLE);
+                bottle.setAmount(1);
+                pice.getPlayer().getInventory().remove(bottle);
+            }, 1L);
+        }
     }
 
 }
